21b6333e94a805d8461e455da31ab2357bacb3ac
Arnav Tiwari <arnav.luck@gmail.com>
2024-12-08 20:30:24 +0530

    db update

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/newStore',).then(() => {
+mongoose.connect('mongodb+srv://arnav:arnav123@cluster0.nnbtmu1.mongodb.net/newStore',).then(() => {
     console.log('Connected to the database!')
 }).catch((err) => {
     console.log('Cannot connect to the database!', err)
